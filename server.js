f7a641b72027b887cce6390415a4ead7ae7a81e8
Lee jeongchan <sjk338285@gmail.com>
2025-04-08 17:21:04 +0900

    inser 테스트

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ let url = require('url');
 function start(route, handle) {
     function onRequest(request, response) {
         let pathname = url.parse(request.url).pathname;
-        
+        let queryData = url.parse(request.url, true).query;
         if (pathname === "/favicon.ico") {
             response.writeHead(204);
             response.end();
             return;
         }
-        route(pathname, handle, response);
+
+        route(pathname, handle, response, queryData.productId);
     }
 
 
     http.createServer(onRequest).listen(7777)
-    // localhost:8888
+    // localhost:7777
 }
 
 exports.start = start;
